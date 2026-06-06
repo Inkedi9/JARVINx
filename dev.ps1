6e03f7736ca1c161572f11f1078cb01aefab2fad
Inkedi9 <kevinho.belot@yahoo.com>
2026-06-07 01:30:40 +0200

    scripts: fix dry-run lancement du runtime

--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,3 @@
 # dev.ps1
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd runtime; go run ./cmd/main.go"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd runtime; go run ./cmd/main.go --dry-run"
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd dashboard; npm run dev"
